9e0bc3718b33031b27adb02e4378c9ebc0458ef4
duo <21155859@qq.com>
2017-09-21 10:43:11 +0800

    monobehaviour
    add monobehaviour demo

---
+++ Assets/Example/Script/SimpleBehavior.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SimpleBehavior : MonoBehaviour {
+
+	public bool ShowGUI;
+	public string Message;
+
+	void Awake()
+	{
+		useGUILayout = ShowGUI;	
+	}
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnGUI()
+	{
+		GUILayout.Label("Hello world");
+		if(GUILayout.Button("Find Component"))
+		{
+			var component = GetComponentInChildren<Text>();
+			if(component != null)
+			{
+				Message = "Find Text";
+			}
+		}
+		GUILayout.Label(Message);
+	}
+}
